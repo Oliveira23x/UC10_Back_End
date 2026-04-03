376d37723b849f6680bf6f35bd84c2157fa3fdb6
Oliveira23x <diegooliveira01.ez@gmail.com>
2026-04-02 22:12:01 -0300

    Criacao das models de Usuario

---
+++ Aula 01/ControleEstoque/ControleEstoque.API/Models/Usuario.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControleEstoque.API.Models
+{
+
+    public enum PerfilUsuario
+    {
+        Cliente,
+        Gerente,
+        Caixa
+    }
+    public abstract class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, StringLength(100)]
+        public string? Nome { get; set; }
+
+        [Required, EmailAddress, StringLength(100)]
+        public string? Email { get; set; }
+
+        [Required]
+        public string? SenhaHash { get; set; } //Aplicar Tamanho mínimo
+
+        [Required]
+        public PerfilUsuario Perfil { get; set; }
+    }
+    public class Cliente : Usuario
+    {
+        [StringLength(14)]
+        public string CPF { get; set; }
+        
+        
+        // Propriedades específicas para Cliente    
+
+    }
+
+    public class Gerente : Usuario
+    {
+        [StringLength(50)]
+        public string Turno { get; set; }
+
+
+        // Propriedades específicas para Gerente
+    }
+
+    public class Caixa : Usuario
+    {
+        [StringLength(50)]
+        public string Setor { get; set; }
+
+        // Propriedades específicas para Caixa
+    }
+}
