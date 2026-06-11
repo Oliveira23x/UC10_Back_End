99b21e7ac8710a061531572c4963122948692947
Diego Oliveira <diegooliveira01.ez@gmail.com>
2026-06-11 20:17:28 -0300

    Delete Aula 01/ControleEstoque directory

--- Aula 01/ControleEstoque/ControleEstoque.API/DTOs/CriarItemPedidoDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ControleEstoque.API.DTOs
-{
-    public class CriarItemPedidoDTO
-    {
-        public int ProdutoId { get; set; }
-        public int Quantidade { get; set; }
-      
-    }
-}
--- Aula 01/ControleEstoque/ControleEstoque.API/DTOs/CriarPedidoDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ControleEstoque.API.DTOs
-{
-    public class CriarPedidoDTO 
-    {
-        public int ClienteId { get; set; }
-         public List<CriarItemPedidoDTO> Itens { get; set; } = new List<CriarItemPedidoDTO>();
-    }
-}
